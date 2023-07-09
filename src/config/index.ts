dca76f33bc3617be4f44088d2d6b93e93c7e8e21
aonaolapo <aonaolapo@getcarbon.co>
2023-07-09 20:22:55 +0100

    Enhance implementation for google signup

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export default {
     db: {
         url: process.env.DATABASE_URL || "mongodb://localhost:27017/unox-api",
@@ -8,21 +11,21 @@ export default {
     },
 
     basicAuth: {
-        user: process.env.AUTH_USER || "apiunoxadmin",
-        password: process.env.AUTH_PASS || "xP%h379U6Pjz#@&&2RA%"
+        user: process.env.AUTH_USER,
+        password: process.env.AUTH_PASS 
     },
 
     google: {
-        clientId: process.env.GOOGLE_CLIENT_ID || "914768259946-fv958gos7nbtn7k080er6r4mmcstaj17.apps.googleusercontent.com",
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET || "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",
-        callbackUrl: process.env.UNOX_API_URL ? `${process.env.UNOX_API_URL}/api/google/redirect` : "http://localhost:4000/api/google/redirect"
+        clientId: process.env.GOOGLE_CLIENT_ID || "",
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+        callbackUrl: `${process.env.UNOX_API_URL}/api/google/redirect`
     },
     
-    sessionSecret: process.env.SESSION_SECRET || "unox_api_session_secret",
+    sessionSecret: process.env.SESSION_SECRET || "",
     saltRounds : 10,
     otpLength: 6,
     tokenLength: 32,
     passLegth: 16,
     otpValidityInMinutes: 5,
-    passwordGenerationCharacters: process.env.PASSWORD_GENERATION_CHARACTERS || "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()"
+    passwordGenerationCharacters: process.env.PASSWORD_GENERATION_CHARACTERS || ""
 }
